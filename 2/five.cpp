8dad5187f2357a0507fda174b69f20467551e5ed
aarotoiv <aaro.toivonen@gmail.com>
2020-01-15 09:53:29 +0200

    created files

---
+++ 2/five.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void sekunnitAjaksi(long sekunnit) {
+
+}
+
+int main() {
+
+
+    return 0;
+}
+
+void sekunnitAjaksi(long sekunnit) {
+    
+}
---
+++ 2/four.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+bool prime(int luku);
+
+int main() {
+
+
+
+    return 0;
+}
+
+bool prime(int luku) {
+    
+}

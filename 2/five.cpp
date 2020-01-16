de3e294df92b39054efe9f309607454bbae6814a
aarotoiv <aaro.toivonen@gmail.com>
2020-01-16 15:17:28 +0200

    ex. 5 done

--- 2/five.cpp
+++ 2/five.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <ctime>
 
-void sekunnitAjaksi(long sekunnit) {
-
-}
+void sekunnitAjaksi(int kaikki_sekunnit, int &tunnit, int &minuutit, int &sekunnit);
 
 int main() {
-
-
+    int sekunnit = 0;
+    int minuutit = 0;
+    int tunnit = 0;
+    std::time_t kaikki_sekunnit = std::time(nullptr) % (24 * 60 * 60);
+    sekunnitAjaksi(kaikki_sekunnit, tunnit, minuutit, sekunnit);
+    std::cout << tunnit << ":" << minuutit << ":" << sekunnit << std::endl;
     return 0;
 }
 
-void sekunnitAjaksi(long sekunnit) {
-    
+void sekunnitAjaksi(int kaikki_sekunnit, int &tunnit, int &minuutit, int &sekunnit) {
+    sekunnit = kaikki_sekunnit % 60;
+    minuutit = kaikki_sekunnit / 60 % 60;
+    tunnit = kaikki_sekunnit / 60 / 60;
 }

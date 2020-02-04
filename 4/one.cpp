9ccb353d043bedc07e0a526fd0b6f8949daf82d7
aarotoiv <aaro.toivonen@gmail.com>
2020-02-04 10:28:49 +0200

    tweaks on ex.1

--- 4/one.cpp
+++ 4/one.cpp
@@ -3,9 +3,13 @@
 int main() {
     const unsigned int kilo = 1000;
     //max
-    char taulukko[kilo * 8379];
-    char * taulukko_p = new char[kilo * 800];
-
+    char taulukko[kilo * 8379] = {'a'};
+    char * taulukko_p = new char[kilo * 1000000000];
+    
+    delete[] taulukko_p;
     std::cout << "ayy" << std::endl;
+    while(true) {
+
+    }
     return 0;
 }
